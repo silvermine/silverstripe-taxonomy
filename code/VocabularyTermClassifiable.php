dfac42c8723a497d95ba3d1d0ed599fe30d47116
Jeremy Thomerson <jeremy@thomersonfamily.com>
2012-05-02 14:26:56 +0000

    Add HasVocabTerm function for use in templates

--- code/VocabularyTermClassifiable.php
+++ code/VocabularyTermClassifiable.php
@@ -29,4 +29,33 @@ class VocabularyTermClassifiable extends DataObjectDecorator {
       ));
    }
 
+   /**
+    * Helper function for templates to see if a particular page has a term in
+    * a particular vocabulary.  Note that since SS (2.4.x) will not parse
+    * <% uf HasVocabName($termMN, $vocabMN) %> we must result to a hack to
+    * allow checking a particular vocabulary and term.  If you need to do this,
+    * use the form <% if HasVocabName(vocMN_termMN) %> where you separate the
+    * vocabulary machine name and term machine name by an underscore and place
+    * the vocabulary machine name first.
+    *
+    * @param string $termMN the term machine name (or both vocab and term - see above)
+    * @param string $vocabMN the vocabulary machine name
+    * @return boolean true if the owner has this term
+    */
+   public function HasVocabTerm($termMN, $vocabMN = '') {
+      if (strpos($termMN, '_') !== false) {
+         $names = explode('_', $termMN);
+         $vocabMN = $names[0];
+         $termMN = $names[1];
+      }
+
+      foreach ($this->owner->VocabularyTerms() as $term) {
+         if ($term->MachineName == $termMN && (empty($vocabMN) || $term->Vocabulary()->MachineName == $vocabMN)) {
+            return true;
+         }
+      }
+
+      return false;
+   }
+
 }
