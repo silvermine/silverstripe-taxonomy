silvermine/silverstripe-taxonomy

commit 72351042751cd5a8763a71c78acd41307767520d

Make vocab and vocab term management UI SS3.1 compatible

code/VocabularyTerm.php
@@ -35,7 +35,6 @@ class VocabularyTerm extends DataObject {
 
    // config for data model admin:
    static $summary_fields = array(
-      'Vocabulary.Name',
       'Term',
       'MachineName',
       'ParentsTerms',
@@ -71,40 +70,48 @@ class VocabularyTerm extends DataObject {
    }
 
    function getCMSFields() {
-      $fields = parent::getCMSFields();
-      $fields->removeByName('Parents');
-      $fields->removeByName('Children');
-      $fields->addFieldToTab('Root.Main', new LiteralField('ManageChildren', '<h3>Manage Children</h3>'));
-      $fields->addFieldToTab(
-         'Root.Main',
-         new ManyManyComplexTableField(
-            $controller = $this,
-            $name = 'Children',
-            $sourceClass = 'VocabularyTerm',
-            $fieldList = array('Term' => 'Term'),
-            $detailFormFields = null,
-            $sourceFilter = sprintf('"VocabularyTerm"."VocabularyID" = %d AND "VocabularyTerm"."ID" <> %d', $this->VocabularyID, $this->ID)
-         )
-      );
-      return $fields;
-   }
+      $fields = new FieldList();
+      $fields->add(new TextField('Term'));
+      $fields->add(new TextField('MachineName'));
 
-   public function getFullTermTitle() {
-      return sprintf('%s : %s', $this->Vocabulary()->Name, $this->Term);
+      if (!$this->ID) {
+         // if we haven't been saved, we don't want to confuse the page
+         // more with children management since you can't actually manage
+         // the children relationships until you are persisted.
+         return $fields;
+      }
+
+      $config = GridFieldConfig_RelationEditor::create($itemsPerPage = 25);
+      // Don't allow creating a new term from this page (at this time).
+      // It is simply for linking to existing ones.  In the future we
+      // could add support for creating children directly from their parent.
+      $config->removeComponentsByType('GridFieldAddNewButton');
+      $config->getComponentByType('GridFieldAddExistingAutocompleter')->setResultsFormat(self::AUTO_COMPLETE_FORMAT);
+
+      $childrenGrid = new GridField(
+         'Children',
+         'Children',
+         $this->Children(),
+         $config
+      );
+      $fields->add($childrenGrid);
+      return $fields;
    }
 
    public function getParentsTerms() {
       return implode(', ', $this->Parents()->map('ID', 'Term')->toArray());
    }
 
-   public function Summary() {
-      $summary = $this->getFullTermTitle();
-      $summary .= sprintf(' [%s:%s]', $this->Vocabulary()->MachineName, $this->MachineName);
-      $parents = $this->getParentsTerms();
-      if (!empty($parents)) {
-         $summary .= sprintf('<em>' . _t('VocabularyTerm.Summary.Parents', ' (Parent terms: %s)') . '</em>', $parents);
-      }
-      return $summary;
+   public function onBeforeDelete() {
+      // don't actually delete them, but remove
+      // the association
+      $this->Children()->removeAll();
+   }
+
+   public function getTitle() {
+      // this is used by the SS admin UI as what shows in breadcrumbs,
+      // successful save/edit messages, etc
+      return SSViewer::fromString(self::AUTO_COMPLETE_FORMAT)->process($this);
    }
 
 }

code/TaxonomyModelAdmin.php
@@ -12,7 +12,6 @@ class TaxonomyModelAdmin extends ModelAdmin {
 
    static $managed_models = array(
       'Vocabulary',
-      'VocabularyTerm',
    );
 
    static $url_segment = 'taxonomy';

code/Vocabulary.php
@@ -41,4 +41,35 @@ class Vocabulary extends DataObject {
       // SS templates need to be able to use the machine name as parameters to "if" statements
       return new RequiredFields('Name', 'MachineName');
    }
+
+   public function getCMSFields() {
+      $fields = new FieldList();
+      $fields->add(new TextField('Name'));
+      $fields->add(new TextField('MachineName'));
+
+      if (!$this->ID) {
+         // if we haven't been saved, we don't want to confuse the page
+         // more with terms management since you can't actually manage
+         // the terms relationships until you are persisted.
+         return $fields;
+      }
+
+      $config = GridFieldConfig_RecordEditor::create($itemsPerPage = 25)
+         ->removeComponentsByType('GridFieldDetailForm')
+         ->addComponent($termDetailForm = new VocabularyTermDetailForm())
+      ;
+      // change button name on "add new"
+      $config->getComponentByType('GridFieldAddNewButton')->setButtonName(_t('Vocabulary.AddNewTermButtonLabel', 'Add new term to this vocabulary'));
+
+      // TODO: after a term is added, you should be taken back to the Vocabulary view or edit screen
+
+      $termsGrid = new GridField(
+         'Terms',
+         'Terms',
+         $this->Terms(),
+         $config
+      );
+      $fields->add($termsGrid);
+      return $fields;
+   }
 }

code/VocabularyTermDetailForm.php
@@ -1,0 +1,28 @@
+<?php
+
+/**
+ * Detail form for adding a term.  Only additional logic we add here is custom
+ * redirect back to vocabulary page after the term is added (instead of the edit
+ * page for the new term, which is confusing because it looks like the form didn't
+ * do anything since you still see your field values of the term you just added
+ * along with a blank list of children).
+ *
+ * @author Jeremy Thomerson <jeremy@thomersonfamily.com>
+ * @copyright (c) 2013 Jeremy Thomerson <jeremy@thomersonfamily.com>
+ * @package silverstripe-taxonomy
+ * @subpackage code
+ */
+class VocabularyTermDetailForm extends GridFieldDetailForm {
+
+   // need this as a placeholder so that we use our own _ItemRequest class below
+
+}
+
+class VocabularyTermDetailForm_ItemRequest extends GridFieldDetailForm_ItemRequest {
+
+   public function doSave($data, $form) {
+      parent::doSave($data, $form);
+      return Controller::curr()->redirect($this->getGridField()->Link());
+   }
+
+}

commit 8ebedc1e525c37900b239651bb5f9e8ab0d55c75

Fixed onBeforeDelete functionality
This was a regression in 72351042751cd5a8763a71c78acd41307767520d

code/VocabularyTerm.php
@@ -103,6 +103,8 @@ class VocabularyTerm extends DataObject {
    }
 
    public function onBeforeDelete() {
+      parent::onBeforeDelete();
+
       // don't actually delete them, but remove
       // the association
       $this->Children()->removeAll();